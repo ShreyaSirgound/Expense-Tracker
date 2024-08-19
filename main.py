72e91a2550d994e44b2399c1d16357be0db3fabf
ShreyaSirgound <95923789+ShreyaSirgound@users.noreply.github.com>
2024-08-19 18:52:25 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 class Expense:
-    def __init__(self, date, description, amount):
+    def __init__(self, date, description, amount, category):
         self.date = date
-        self.desciption = description
+        self.description = description
         self.amount = amount
+        self.category = category
 
 class ExpenseTracker:
     def __init__(self):
         self.expenses = []
+        self.categories  = []
 
     def add_expense(self, expense):
         self.expenses.append(expense)
+
+    def print_categories(self):
+        print("Categories: ")
+        for i, category in enumerate(self.categories, start=1):
+            print(f"{i}. {category}")
     
     def remove_expense(self, index):
         if 0 <= index < len(self.expenses):
@@ -24,7 +31,7 @@ class ExpenseTracker:
         else:
             print("Expense List:")
             for i, expense in enumerate(self.expenses, start=1):
-                print(f"{i}. Date: {expense.date}, Description: {expense.description}, Amount: {expense.amount}")
+                print(f"{i}. Date: {expense.date}, Description: {expense.description}, Amount: {expense.amount}, Category: {expense.category}")
 
     def total_expenses(self):
         total = sum(expense.amount for expense in self.expenses)
@@ -37,26 +44,34 @@ def main():
         print("\nExpense Tracker Menu:")
         print("1. Add Expense")
         print("2. Remove Expense")
-        print("3. View Expenses")
-        print("5. Exit")
+        print("3. Create a Category")
+        print("4. View All Expenses")
+        print("5. Total Expenses")
+        print("6. Exit")
 
         choice = input("Enter your choice (1-5): ")
 
         if choice == "1":
             date = input("Enter the date (YYYY-MM-DD): ")
             description = input("Enter the desciption: ")
-            amount = float(input("Enter the amount: "))
-            expense = Expense(date, description, amount)
+            amount = float(input("Enter the amount ($): "))
+            tracker.print_categories()
+            category = int(input("Enter a category number: ")) - 1
+            expense = Expense(date, description, amount, tracker.categories[int(category)])
             tracker.add_expense(expense)
             print("Expense added successfully.")
         elif choice == "2":
             index = int(input("Enter the expense index to remove: ")) - 1
             tracker.remove_expense(index)
         elif choice == "3":
+            category_name = input("Enter category name: ")
+            tracker.categories.append(category_name)
+            print("Category added sucessfully.")
+        elif choice == "4":
             tracker.view_expenses()
-        elif choice =="4":
-            tracker.total_expenses()
         elif choice == "5":
+            tracker.total_expenses()
+        elif choice == "6":
             print("Goodbye!")
             break
         else:
